b2f345ced593e33d4695b14d5fb4ae386d1a7442
Raghul-7 <r.raghulkrishna@resulticksmail.com>
2024-11-28 15:42:30 +0530

    change: document.js"sdk", importScript for firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
-import "../styles/index.css";
-// import Script from 'next/script'
-
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      {/* <Script fcm_service_path="/firebase-messaging-sw.js" src="https://sdk.smartdx.co/handlers/d91a8e6c82fe4cdba9a80f5040699a4c.sdk" defer="defer" /> */}
-    </>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <script
           fcm_service_path="/firebase-messaging-sw.js"
-          src="https://sdk.smartdx.co/handlers/d91a8e6c82fe4cdba9a80f5040699a4c.sdk"
+          src="https://sdk.smartdx.co/handlers/d91a8e6c_82fe_4cdb_a9a8_0f5040699a4c.sdk"
           defer="defer"
         />
       </Head>
